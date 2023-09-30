866d408fcecf1de7c9a20349d9cde2d9be407fb7
hemantnemade2018 <46903798+hemantnemade2018@users.noreply.github.com>
2023-09-30 00:21:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.txt('Omega 3 & Bluebeery')
+streamlit.txt('Hard-Boiled Free-Range')
 
